d906ac3347d1d16f72b619c133c5e57b709b9cc3
v01d <phreakuencies@gmail.com>
2012-01-10 19:52:06 +0100

    CMakeLists fixes

--- metric_map.cpp
+++ metric_map.cpp
@@ -6,7 +6,7 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/core/core_c.h>
 #include <opencv2/imgproc/imgproc.hpp>
-#include <highgui.h>
+#include <opencv2/highgui/highgui.hpp>
 #include <sys/stat.h>
 #include "metric_map.h"
 #include "util.h"
