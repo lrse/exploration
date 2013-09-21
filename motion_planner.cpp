671e509dca57e28eaa982048de8bebef4c369c45
v01d <phreakuencies@gmail.com>
2013-09-21 19:18:17 +0200

    fix

--- motion_planner.cpp
+++ motion_planner.cpp
@@ -28,7 +28,7 @@ MotionPlanner::MotionPlanner(PlayerCc::PlayerClient* client_proxy) : Singleton<M
 void MotionPlanner::set_goal(double x, double y, double theta)
 {
   cout << "setting pose to: " << x << " " << y << " " << theta << endl;
-  #ifdef SYROTEK
+  #ifdef ENABLE_SYROTEK
   double current_angle = ExaBot::instance()->position_proxy.GetYaw();
   double delta_angle = gsl_sf_angle_restrict_symm(theta - current_angle);
   if (fabsf(delta_angle) > REACHED_ANGLE_EPSILON)
